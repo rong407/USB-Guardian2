a3ec8c6c0dff8da737970e2eea9e21c8abfaf66b
E3X.AI <35677190+rong407@users.noreply.github.com>
2026-04-04 07:03:32 +0700

    Change SERVER_URL to local development address

--- api_client.py
+++ api_client.py
@@ -1,7 +1,6 @@
 import requests
 
-#SERVER_URL = "http://127.0.0.1:8000/search"
-SERVER_URL = "http://104.218.52.181:8000/search"
+SERVER_URL = "http://127.0.0.1:8000/search"
 
 def search_by_hash(file_hash):
 
